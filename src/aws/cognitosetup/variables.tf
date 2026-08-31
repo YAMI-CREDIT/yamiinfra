901eb867e347c990e1e7a665c4b7e08cf2021e12
Adewale Opeloyeru <opeloyeruadewale@icloud.com>
2026-08-31 13:53:07 +0100

    chore: setup cognito

--- src/aws/cognitosetup/variables.tf
+++ src/aws/cognitosetup/variables.tf
@@ -1,4 +1,5 @@
 variable "backend_user_registration_url" {
   description = "HTTPS endpoint on backend that receives confirmed user registration"
   type        = string
+  default     = "https://yamicredit.com/api/v1/users"
 }
